2eec2ce49c6714b2c1efbf880b1209cdac55d14c
gmzelias <gmzelias@gmail.com>
2018-01-16 15:05:02 -0400

    Changed server.js

--- server.js
+++ server.js
@@ -1,76 +1,62 @@
 const express = require('express');
-const hbs = require ('hbs');
+const hbs = require('hbs');
 const fs = require('fs');
+
 const port = process.env.PORT || 3000;
 var app = express();
 
+hbs.registerPartials(__dirname + '/Views/Partials')
+app.set('view engine', 'hbs');
 
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
-hbs.registerPartials(__dirname + '/Views/Partials');
-app.set('view engine','hbs');
-
-
-app.use((req, res, next)=>{
-    var now = new Date().toString();
-    var log =`${now}:${req.method} ${req.url}`;
-    console.log(log);
-    fs.appendFile('Server.log', log + '\n', (err)=>{
-        if (err){
-            console.log('Unable to create Server.log');
-        }
-    });
-next();
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
 });
 
-/*app.use((req,res,next)=>{
-res.render('maintence.hbs');
-next();
-});*/
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
-app.use(express.static(__dirname+'/Public'));
+app.use(express.static(__dirname + '/Public'));
 
-hbs.registerHelper('getCurrentYear', () =>{
-    return new Date().getFullYear();
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt', (text) =>{
-    return text.toUpperCase();
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
 });
 
-app.get('/',(req,res)=>{
-/*res.send('<h1>Hello Express!<h1>');
-res.send({
-    name: 'Elias',
-    likes:[
-       'Gin',
-       'Baseball' 
-    ]
-});*/
-res.render('home',{
-    pageTitle : 'Home',
-    welcomeMessage:'Welcome to my website'
-
-})
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
 });
 
-app.get('/about',(req,res)=>{
-res.render('about.hbs',{
-    pageTitle : 'About Page'
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
 });
 
-app.get('/bad',(req,res)=>{
-    res.send({
-        error:'Unable to fulfill request'
-    });
-    });
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
-app.get('/projects',(req,res)=>{
-    res.render('projects.hbs',{
-        pageTitle : 'Projects'
-    });
-        });
-
-app.listen(port,()=>{
-    console.log(`Server is up and running on port ${port}`)
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
