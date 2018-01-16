d8e3f42047755d9f6675c19e8b784116d364b969
gmzelias <gmzelias@gmail.com>
2018-01-16 15:11:02 -0400

    udemy test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/Views/Partials')
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
 
-app.use(express.static(__dirname + '/Public'));
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
